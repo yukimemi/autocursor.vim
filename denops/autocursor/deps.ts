defc37260062d6136eed62670f5faaa9b411942c
yukimemi <yukimemi@gmail.com>
2021-07-10 21:56:49 +0900

    :bug: Change lock point.

--- denops/autocursor/deps.ts
+++ denops/autocursor/deps.ts
@@ -1,7 +1,7 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.0/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.0/autocmd/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.0/variable/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.0/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.1/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.1/autocmd/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.1/variable/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.1/helper/mod.ts";
 
 export { ensureNumber } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
 
