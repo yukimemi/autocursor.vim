5e0f90a72e2eb94c6d02def862f247b81d2212cb
yukimemi <yukimemi@gmail.com>
2021-07-11 22:35:30 +0900

    :up: Update denops_std.

--- denops/autocursor/deps.ts
+++ denops/autocursor/deps.ts
@@ -1,7 +1,7 @@
-export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.1/mod.ts";
-export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.1/autocmd/mod.ts";
-export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.1/variable/mod.ts";
-export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.1/helper/mod.ts";
+export type { Denops } from "https://deno.land/x/denops_std@v1.0.0-beta.5/mod.ts";
+export * as autocmd from "https://deno.land/x/denops_std@v1.0.0-beta.5/autocmd/mod.ts";
+export * as vars from "https://deno.land/x/denops_std@v1.0.0-beta.5/variable/mod.ts";
+export { execute } from "https://deno.land/x/denops_std@v1.0.0-beta.5/helper/mod.ts";
 
 export { ensureNumber } from "https://deno.land/x/unknownutil@v0.1.1/mod.ts";
 
